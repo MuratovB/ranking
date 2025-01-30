525323cc17722d7a12306231120605451e1a6cf4
Bektur <muratov_b@auca.kg>
2025-01-30 11:27:17 +0000

    Trying to fix scrapping

--- api/main.py
+++ api/main.py
@@ -5,4 +5,4 @@ app = FastAPI()
 
 @app.get('/')
 def home():
-  return get_video_links()
+  return len(get_video_links())
--- scrap_playlist.py
+++ scrap_playlist.py
@@ -1,3 +1,4 @@
+c='''
 from pytube import Playlist
 
 def get_video_links(playlist_url='https://www.youtube.com/playlist?list=PLItAIW8MhCBa2BQYeTK9kWGSRJ5jFn8CG'):
@@ -7,4 +8,18 @@ def get_video_links(playlist_url='https://www.youtube.com/playlist?list=PLItAIW8
     # Extract all video URLs in the playlist
     video_urls = playlist.video_urls
     
+    return video_urls
+'''
+
+import yt_dlp
+
+def get_video_links(playlist_url='https://www.youtube.com/playlist?list=PLItAIW8MhCBa2BQYeTK9kWGSRJ5jFn8CG'):
+    ydl_opts = {
+        'extract_flat': True,  # Avoid downloading videos, just get the links
+        'quiet': True,
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info_dict = ydl.extract_info(playlist_url, download=False)
+        video_urls = [entry['url'] for entry in info_dict['entries']]
     return video_urls
